42a3d4d26ba2dafd4d6b6a98ad1f54f0fa80b8c2
guillermorangel <guillermo.m.rangel@gmail.com>
2021-05-07 18:39:02 -0400

    mac setup files

---
+++ casks.sh
@@ -0,0 +1,33 @@
+# Tools
+brew install --cask boom
+brew install --cask flux
+brew install --cask lunar
+brew install --cask rectangle
+
+# Dev
+brew install --cask github
+brew install --cask gitkraken
+brew install --cask figma
+brew install --cask iterm2
+brew install --cask tableplus
+brew install --cask slack
+brew install --cask visual-studio-code
+
+# Utils
+brew install --cask appcleaner
+brew install --cask bitwarden
+brew install --cask daisydisk
+brew install --cask grandperspective
+brew install --cask keybase
+
+# Apps
+brew install --cask adobe-creative-cloud
+brew install --cask chrome
+brew install --cask dropbox
+brew install --cask firefox
+brew install --cask megasync
+brew install --cask notion
+brew install --cask obsidian
+brew install --cask spotify
+
+# typeeto
---
+++ ruby.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Ruby
+brew install ruby-install
+ruby-install
+ruby-install ruby # Installs the latest and greatest
+brew install chruby --HEAD
+chruby
