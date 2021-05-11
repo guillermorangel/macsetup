53cc00e7697f55f6856f288354d66e45a37c81c8
guillermorangel <guillermo.m.rangel@gmail.com>
2021-05-11 01:29:55 -0400

    alphabetize vscode extensions

--- vs_code.sh
+++ vs_code.sh
@@ -3,25 +3,26 @@
 # VSCode Extensions
 
 brew install --cask visual-studio-code
-code --install-extension sleistner.vscode-fileutils 
 code --install-extension eamodio.gitlens
-code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension esbenp.prettier-vscode
+code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension sleistner.vscode-fileutils 
 
 # Ruby
-code --install-extension castwide.solargraph
 code --install-extension bung87.rails
+code --install-extension bung87.vscode-gemfile
+code --install-extension castwide.solargraph
 code --install-extension elliotlarson.ruby-around-the-block
 code --install-extension misogi.ruby-rubocop
 code --install-extension noku.rails-run-spec-vscode
 code --install-extension rebornix.ruby
-code --install-extension bung87.vscode-gemfile
 
-code --install-extension christian-kohler.path-intellisense
-code --install-extension anweber.vscode-tidyhtml
-code --install-extension dbaeumer.vscode-eslint
+# Other
 code --install-extension abusaidm.html-snippets
-code --install-extension mrmlnc.vscode-scss
+code --install-extension anweber.vscode-tidyhtml
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension icao.electron-vue
 code --install-extension github.github-vscode-theme
+code --install-extension mrmlnc.vscode-scss
