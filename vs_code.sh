9d46403d7876420a4a50d4fa5f16886b5915a319
guillermorangel <guillermo.m.rangel@gmail.com>
2021-05-27 15:23:13 -0400

    Add stylelinet vscode extension, and organize

--- vs_code.sh
+++ vs_code.sh
@@ -3,8 +3,10 @@
 # VSCode Extensions
 
 brew install --cask visual-studio-code
+
+# General
+code --install-extension christian-kohler.path-intellisense
 code --install-extension eamodio.gitlens
-code --install-extension esbenp.prettier-vscode
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension sleistner.vscode-fileutils 
@@ -18,14 +20,19 @@ code --install-extension misogi.ruby-rubocop
 code --install-extension noku.rails-run-spec-vscode
 code --install-extension rebornix.ruby
 
-# Other
+# Formatting & Linting
 code --install-extension abusaidm.html-snippets
 code --install-extension anweber.vscode-tidyhtml
-code --install-extension christian-kohler.path-intellisense
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension mrmlnc.vscode-scss
+code --install-extension stylelint.vscode-stylelint
+
+# Themes
 code --install-extension icao.electron-vue
 code --install-extension github.github-vscode-theme
-code --install-extension mrmlnc.vscode-scss
+
+
 
 # Setup VSCode
 
